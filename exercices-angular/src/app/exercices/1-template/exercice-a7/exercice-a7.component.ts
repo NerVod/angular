0eb8aa2800c5191bde6c2de2cffbd64ede467085
NerVod <benjamin.jeannerot@gmail.com>
2022-02-02 15:13:47 +0100

    exo 7 angular

--- exercices-angular/src/app/exercices/1-template/exercice-a7/exercice-a7.component.ts
+++ exercices-angular/src/app/exercices/1-template/exercice-a7/exercice-a7.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExerciceA7Component implements OnInit {
 
+  voiture = [
+    {
+      modele: "Peugeot e Deux cent huit",
+      puissance: "260Nm",
+      prix: 37000,
+      dateFabrication: Date.now()
+    },
+    {
+      modele: "Renault Arkana",
+      puissance: "140ch",
+      prix: 36000,
+      dateFabrication: Date.now() 
+    },
+    {
+      modele: "Volkswagen Tiguan",
+      puissance: "150CV",
+      prix: 55000,
+      dateFabrication: Date.now() 
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
